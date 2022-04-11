3af32f47c903918ed0d14284c7747d924569da31
keydelk <keydelk@gmail.com>
2022-04-11 02:15:36 +0000

    compound words problem solved

--- compound_words/cw.c
+++ compound_words/cw.c
@@ -13,10 +13,12 @@ Output: each compound word, one per line, in alphabetical
         order.
 =======================================================*/
 
-#import <stdio.h>
-#import <stdlib.h>
-#import <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define hashsize(n) ((unsigned long)1 << (n))
+#define hashmask(n) (hashsize(n) - 1);
 #define WORD_LENGTH 16
 #define NUM_BITS 17
 
@@ -26,8 +28,39 @@ typedef struct word_node {
 } word_node;
 
 char *read_line(int size);
+unsigned long oaat(char *key, unsigned long len, unsigned long bits);
+int in_hash_table(word_node *hash_table[], char *find, unsigned find_len);
+void indentify_compound_words(char *words[], word_node *hash_table[], int total_words);
 
 int main() {
+    static char *words[1 << NUM_BITS] = {NULL};
+    static word_node *hash_table[1 << NUM_BITS] = {NULL};
+    int total = 0;
+    char *word;
+    word_node *wordptr;
+    unsigned length, word_code;
+
+    do {
+        word = read_line(WORD_LENGTH);
+        words[total] = word;
+        wordptr = malloc(sizeof(word_node));
+        if (wordptr == NULL){
+            fprintf(stderr, "malloc error\n");
+            exit(1);
+        }
+        length = strlen(word);
+        word_code = oaat(word, length, NUM_BITS);
+        wordptr->word = &words[total];
+        wordptr->next = hash_table[word_code];
+        hash_table[word_code] = wordptr;
+        total++;
+    } while(*word);
+
+    indentify_compound_words(words, hash_table, total);
+
+    for(int i = 0; i < total; i++) {
+        free(words[i]);
+    }
 
     return EXIT_SUCCESS;
 }
@@ -56,3 +89,43 @@ char *read_line(int size) {
     return str;
 }
 
+unsigned long oaat(char *key, unsigned long len, unsigned long bits) {
+    unsigned long hash, i;
+
+    for (hash = 0, i = 0; i < len; i++) {
+        hash += key[i];
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    return hash & hashmask(bits);
+}
+
+int in_hash_table(word_node *hash_table[], char *find, unsigned find_len) {
+    unsigned word_code;
+    word_node *wordptr;
+    word_code = oaat(find, find_len, NUM_BITS);
+    wordptr = hash_table[word_code];
+
+    while (wordptr) {
+        if ((strlen(*(wordptr->word)) == find_len) && (strncmp(*(wordptr->word), find, find_len) == 0))
+            return 1;
+        wordptr = wordptr->next;
+    }
+    return 0;
+}
+
+void indentify_compound_words(char *words[], word_node *hash_table[], int total_words) {
+    unsigned len;
+    for (int i = 0; i < total_words; i++) {
+        len = strlen(words[i]);
+        for (int j = 1; j < len; j++) {
+            if (in_hash_table(hash_table, words[i], j) && in_hash_table(hash_table, &words[i][j], len - j)) {
+                printf("%s\n", words[i]);
+                break;
+            }
+        }
+    }
+}
