152bcad588bcc8130d6039a35b2d86187828859a
Keith Keydel <keydelk@mntre-builder>
2022-05-25 08:24:30 -0600

    Book translation: main and read word functions

--- book_translation/bt.c
+++ book_translation/bt.c
@@ -31,14 +31,84 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_TARGETS 100
+#define MAX_LANGS 101
+#define WORD_LENGTH 16
 #define MAX_TRANSLATORS 4500
 
+typedef struct edge {
+	int to_lang, cost;
+	struct edge *next;
+} edge;
 
+typedef int board[MAX_LANGS];
+typedef int positions[MAX_LANGS];
 
-int main() {
-	printf("Max target languages: %d\nMax translators: %d\n", MAX_TARGETS, MAX_TRANSLATORS);
+char *read_word(int size);
+
+int main(void) {
+	static edge *adj_list[MAX_LANGS] = {NULL};
+	static char *lang_names[MAX_LANGS];
+	int i, num_targets, num_translators, cost, from_index, to_index;
+	char *from_lang, *to_lang;
+	edge *e;
+	static board min_costs;
+	scanf("%d %d\n", &num_targets, &num_translators);
+	lang_names[0] = "English";
+
+	for (i = 1; i <= num_targets; i++)
+		lang_names[i] = read_word(WORD_LENGTH);
+
+	for (i = 0; i < num_translators; i++) {
+		from_lang = read_word(WORD_LENGTH);
+		to_lang = read_word(WORD_LENGTH);
+		scanf("%d\n", &cost);
+		from_index = find_lang(lang_names, from_lang);
+		to_index = fing_lang(lang_names, to_lang);
+		e = malloc(sizeof(edge));
+		if (e == NULL) {
+			fprintf(stderr, "malloc error\n");
+			exit(EXIT_FAILURE);
+		}
+		e->to_lang = to_index;
+		e->cost = cost;
+		e->next = adj_list[from_index];
+		adj_list[from_index] = e;
+		e = malloc(sizeof(edge));
+		if (e == NULL) {
+			fprintf(stderr, "malloc error\n");
+			exit(EXIT_FAILURE);
+		}
+		e->to_lang = from_index;
+		e->cost = cost;
+		e->next = adj_list[to_index];
+		adj_list[to_index] = e;
+	}
+	find_distances(adj_list, num_targets + 1, min_costs);
+	solve(num_targets + 1, min_costs);
 
 	return EXIT_SUCCESS;
 }
 
+char *read_word(int size) {
+	char *str;
+	int ch;
+	int len = 0;
+	str = malloc(size);
+	if (str == NULL) {
+		fprintf(stderr, "malloc error\n");
+		exit(EXIT_FAILURE);
+	}
+	while ((ch = getchar()) != EOF && (ch != ' ') && (ch != '\n')) {
+		str[len++] = ch;
+		if (len == size) {
+			size = size * 2;
+			str = realloc(str, size);
+			if (str == NULL) {
+				fprintf(stderr, "realloc error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	str[len] = '\0';
+	return str;
+}
